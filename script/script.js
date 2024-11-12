31748ef30f6ae89a7af726ce37e6b98e7c7a10ad
Przemek B <33911808+kisielo85@users.noreply.github.com>
2024-11-12 13:20:16 +0100

    canvas tools - work in progress

--- script/script.js
+++ script/script.js
@@ -1,23 +1,66 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-let = isDown=false
-let pos={x:0, y:0}
+let = isDown = false
 
-function mouseDown(){
-    isDown=true
-    drawSquare()
+let pos = { x: 0, y: 0 }
+let offset = { x: canvas.offsetLeft, y: canvas.offsetTop }
 
-}
-function mouseUp(){ isDown=false }
-function mouseMove(){
-    pos.x = event.clientX;
-    pos.y = event.clientY;
-    if (isDown){drawSquare()}
+let tool = {
+    type: "brush",
+    width: 20,
+    color: "green",
+    shape: "circle", //circle, square
 }
 
-function drawSquare(width=2){
-    x=pos.x
-    y=pos.y
-    ctx.fillStyle = "red";
-    ctx.fillRect(x-width, y-width, 2*width, 2*width);
+ctx.lineWidth = tool.width;
+ctx.strokeStyle = tool.color
+ctx.fillStyle = tool.color
+
+function mouseDown() {
+    isDown = true
+    switch (tool.type) {
+        case "brush":
+            drawPoint(pos.x, pos.y);
+            ctx.beginPath()
+            break;
+    }
+}
+function mouseUp() {
+    isDown = false
+    ctx.stroke()
+}
+
+function mouseMove(event) {
+    old_x=pos.x
+    old_y=pos.y
+    pos.x = event.clientX - offset.x;
+    pos.y = event.clientY - offset.y;
+    if (!isDown) { return }
+    drawLine(old_x,old_y,pos.x,pos.y)
+}
+
+function drawPoint(x, y) {
+    w = tool.width
+    x = pos.x
+    y = pos.y
+    switch (tool.shape) {
+        case "square":
+            ctx.fillRect(x - w / 2, y - w / 2, w, w);
+            break;
+        case "circle":
+            ctx.beginPath()
+            ctx.arc(x, y, 1, 0, Math.PI * 2, true)
+            ctx.stroke()
+            break;
+    }
+
+}
+
+function drawLine(x1,y1,x2,y2) {
+    ctx.beginPath()
+    ctx.lineCap = "round";
+    ctx.moveTo(x1,y1)
+    ctx.lineTo(pos.x, pos.y)
+    ctx.stroke()
+
 }
