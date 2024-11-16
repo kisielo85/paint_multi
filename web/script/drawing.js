5fa983e223a31c8f8d2c7c66d164d7788f53dfb2
Przemek B <33911808+kisielo85@users.noreply.github.com>
2024-11-16 02:01:29 +0100

    przygotowanie funkcji do wysyłania / odbierania danych

--- web/script/drawing.js
+++ web/script/drawing.js
@@ -1,11 +1,14 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+ctx.lineCap = "round";
+
 let = isDown = false
 
 let cache_pos = { x: 0, y: 0 }
 let pos = { x: 0, y: 0 }
 let offset = {}
-
+let bufferPoints=[]
+let buffercount=0
 let tool = { type: "brush", size: 5, color: "black" }
 
 function mouseDown() {
@@ -26,13 +29,24 @@ function mouseDown() {
 }
 
 function mouseUp() {
+    ctx.beginPath()
+
+    ctx.strokeStyle = tool.color
     isDown = false
     switch (tool.type) {
         case "rect":
             drawRect(cache_pos, pos)
+            sendData()
             break;
         case "line":
             drawLine(cache_pos, pos)
+            sendData()
+        case "brush":
+        case "eraser":
+            if (buffercount==0) break
+            sendData(bufferPoints)
+            bufferPoints=[]
+            buffercount=0
     }
 }
 
@@ -44,10 +58,18 @@ function mouseMove(event) {
 
     switch (tool.type) {
         case "brush":
-            drawLine(old_pos, pos)
-            break;
         case "eraser":
-            drawLine(old_pos, pos, true)
+            drawLine(old_pos, pos, tool.type=="eraser")
+            
+            // zapisuje ścieżke rysowania, i raz na jakiś czas ją wysyła
+            if (buffercount==0) bufferPoints=[old_pos.x,old_pos.y]
+            bufferPoints.push([pos.x,pos.y])
+            buffercount+=1
+            if (buffercount>=49){
+                sendData(bufferPoints)
+                bufferPoints=[]
+                buffercount=0
+            }
             break;
     }
 }
@@ -55,8 +77,6 @@ function mouseMove(event) {
 function drawLine(from, to, eraser = false) {
     if (eraser) { ctx.globalCompositeOperation = "destination-out"; }
     ctx.beginPath()
-    ctx.lineWidth = tool.size;
-    ctx.lineCap = "round";
     ctx.moveTo(from.x, from.y)
     ctx.lineTo(to.x, to.y)
     ctx.stroke()
@@ -68,23 +88,59 @@ function drawRect(from, to) {
     ctx.stroke()
 }
 
-
+// zmiana narzędzia
 function setTool({ type = false, size = false, color = false }) {
     if (type) { tool.type = type }
     if (size) {
         tool.size = size
-        ctx.lineWidth = size;
+        ctx.lineWidth = size
     }
     if (color) {
         tool.color = color
         ctx.strokeStyle = color
-        ctx.fillStyle = color
     }
 }
 setTool({ type: "brush", size: 15, color: "black" })
 
 
+// jakby sie rozmiar okna zmienił
 function win_resize(){
     offset = { x: canvas.offsetLeft, y: canvas.offsetTop }
 }
 win_resize()
+
+// wysyłanie danych
+function sendData(points=[[cache_pos.x,cache_pos.y],[pos.x,pos.y]],type=tool.type, size=tool.size, color=tool.color){
+    if (type=="rect"){
+        points[1][0]-=points[0][0]
+        points[1][1]-=points[0][1]
+    }
+    //console.log(points,type,size,color)
+    //receiveData(points,type,4,"red")
+}
+
+// odbieranie ruchów innych graczy
+function receiveData(p,type,size,color){
+    if (type=="eraser") { ctx.globalCompositeOperation = "destination-out"; }
+    ctx.strokeStyle = color
+    ctx.lineWidth = size
+
+    ctx.beginPath()
+    switch (type){
+        case "rect":
+            ctx.rect(p[0][0], p[0][1], p[1][0], p[1][1])
+            break;
+        case "brush":
+        case "eraser":
+        case "line":
+            ctx.moveTo(p[0][0], p[0][1])
+            for (let i=1; i<p.length; i++){
+                ctx.lineTo(p[i][0],p[i][1])
+            }
+    }
+    ctx.stroke()
+
+    ctx.strokeStyle = tool.color
+    ctx.lineWidth = tool.size
+    ctx.globalCompositeOperation = "source-over"
+}
