3ef5c5072397f1fb90c7dd86ebd152b36742aa46
Marcel Mikulski <100922591+Le-programer@users.noreply.github.com>
2024-11-19 02:42:53 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const serverURL = "localhost:2140"
-//const serverURL = "paint_multi.153070065.xyz"
+//const serverURL = "localhost:2140"
+const serverURL = "paint_multi.153070065.xyz"
 
 const fullURL = `${(serverURL.startsWith("127.0.0.1") || serverURL.startsWith("localhost") ? "http" : "https")}://${serverURL}`
